54ac6532ca0992f8c2aafdefa8172a4818e33bf7
Eric Mauro <dyermauro@outlook.com>
2021-04-29 21:17:43 -0500

    Modified sitemap API route

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -9,38 +9,38 @@ export default function handler(req, res) {
 	const xml = `<?xml version='1.0' encoding='utf-8'?>
       <urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9/'>
       <url>
-      <loc>http://www.dyermaurolaw.com/</loc>
+      <loc>https://www.dyermaurolaw.com/</loc>
       <lastmod>4/29/2021</lastmod>
       <priority>1</priority>
       </url>
       <url>
-      <loc>http://www.dyermaurolaw.com/about</loc>
+      <loc>https://www.dyermaurolaw.com/about</loc>
       <lastmod>4/29/2021</lastmod>
       <priority>0.8</priority>
       </url>
       <url>
-      <loc>http://www.dyermaurolaw.com/contact</loc>
+      <loc>https://www.dyermaurolaw.com/contact</loc>
       <lastmod>4/29/2021</lastmod>
       <priority>0.8</priority>
       </url>
       <url>
-      <loc>http://www.dyermaurolaw.com/business-formation</loc>
+      <loc>https://www.dyermaurolaw.com/business-formation</loc>
       <lastmod>4/29/2021</lastmod>
       </url>
       <url>
-      <loc>http://www.dyermaurolaw.com/family</loc>
+      <loc>https://www.dyermaurolaw.com/family</loc>
       <lastmod>4/29/2021</lastmod>
       </url>
       <url>
-      <loc>http://www.dyermaurolaw.com/noncomplete-nda</loc>
+      <loc>https://www.dyermaurolaw.com/noncomplete-nda</loc>
       <lastmod>4/29/2021</lastmod>
       </url>
       <url>
-      <loc>http://www.dyermaurolaw.com/probate</loc>
+      <loc>https://www.dyermaurolaw.com/probate</loc>
       <lastmod>4/29/2021</lastmod>
       </url>
       <url>
-      <loc>http://www.dyermaurolaw.com/trade-secrets</loc>
+      <loc>https://www.dyermaurolaw.com/trade-secrets</loc>
       <lastmod>4/29/2021</lastmod>
       </url>
       <url>
